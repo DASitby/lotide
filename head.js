14ce8a1bda647d826c17c2d39bc35ac105a68ee0
DASitby <davidsooley551@gmail.com>
2022-11-16 03:35:22 +0000

    linted head.js

--- head.js
+++ head.js
@@ -6,10 +6,10 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const head = function(input){
-  let output = input[0]
-  return output
-}
+const head = function(input) {
+  let output = input[0];
+  return output;
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

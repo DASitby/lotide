fddb5d347175830e4ea3fa0f414f718972ad0dec
DASitby <davidsooley551@gmail.com>
2022-11-16 04:02:04 +0000

    made tail selector function

---
+++ tail.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(input) {
+  let output = input;
+  output.slice(1);
+  return output;
+};
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!

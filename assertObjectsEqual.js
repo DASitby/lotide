3166f1d7a289c2f5621fa149378a312ec652f59a
DASitby <davidsooley551@gmail.com>
2022-11-23 04:06:59 +0000

    added eqArrays function and assigned test code objects to variables, resulting in a success. note: always set test objects to variables to enter them in functions

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,23 @@
+const eqArrays = function(arr1, arr2) {
+  let results = [];
+  let output = false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      results[i] = true;
+    } else {
+      results[i] = false;
+    }
+  }
+  for (let i = 0; i < results.length; i++) {
+    if (results[i] === true) {
+      output = true;
+    } else if (results[i] === false) {
+      output = false;
+      break;
+    }
+  }
+  return output;
+};
 const eqObjects = function(objA, objB) {
   let output;
   for (let key in objA) {
@@ -6,15 +26,15 @@ const eqObjects = function(objA, objB) {
         output = true;
       } else {
         output = false;
-        break
+        break;
       }
     } else if (typeof objA[key] === 'object') {
       if (typeof objB[key] === 'object') {
-        if(objA[key].length === objB[key].length) {
-          output = eqArrays(objA[key], objB[key])
+        if (objA[key].length === objB[key].length) {
+          output = eqArrays(objA[key], objB[key]);
         } else {
-          output = false
-          break
+          output = false;
+          break;
         }
       }
     }
@@ -27,12 +47,13 @@ const eqObjects = function(objA, objB) {
 
 const assertObjectsEqual = function(obj1, obj2) {
   const inspect = require('util').inspect;
-  if (eqObjects(obj1, obj2) === true) {
+  if (eqObjects(obj1, obj2)) {
     return `✅Assertion Passed: ${inspect(obj1)} === ${inspect(obj2)}`;
   } else {
     return `🛑Assertion Failed: ${inspect(obj1)} !== ${inspect(obj2)}`;
   }
 };
 
-
-console.log(assertObjectsEqual({a:1, b:2, bb:3, c:4,},{a:1, b:2, bb:3, c:4,}))
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(assertObjectsEqual(ab, ba));

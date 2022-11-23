70197cac9cbb82b6dd0650fe8490ac2ccab1d7d2
DASitby <davidsooley551@gmail.com>
2022-11-23 03:08:46 +0000

    implemented function for primitive outputs

--- eqObjects.js
+++ eqObjects.js
@@ -5,3 +5,25 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const eqObjects = function(objA, objB) {
+  let output
+  for (let key in objA) {
+    if (objA[key] === objB[key]) {
+      output = true
+    } else {
+      output = false
+    }
+  }
+  if (Object.keys(objA).length !== Object.keys(objB).length){
+    output = false
+  }
+  return output
+}
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);

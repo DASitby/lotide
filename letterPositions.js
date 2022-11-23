91321d6785908b7d8b192cdbb377309787bbcdd8
DASitby <davidsooley551@gmail.com>
2022-11-23 02:18:57 +0000

    copied the code from countLetters function and did a simple refactor in line 5 to  turn the values assigned during the initial key assignment to empty arrays instead, and changed the iteration in line 11 to a .push()

---
+++ letterPositions.js
@@ -0,0 +1,24 @@
+const letterPositions = function(sentence) {
+  let output = {}
+  for (let i = 0; i < sentence.length; i++) {
+    if (output[sentence[i]] === undefined) {
+      output[sentence[i]] = []
+    }
+  }
+  for (let i = 0; i < sentence.length; i++) {
+    for (let key in output) {
+      if (key === sentence[i]) {
+        output[sentence[i]].push(i);
+      }
+    }
+  }
+  for (let x in output) {
+    if (x === ' ') {
+      delete output[x];
+    }
+  }
+  return output
+}
+
+
+console.log(letterPositions("lighthouse in the house"))

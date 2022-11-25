6c3e792338fa28dd23b3e6597b6491f45730e9ee
DASitby <davidsooley551@gmail.com>
2022-11-25 22:32:03 +0000

    linted

--- takeUntil.js
+++ takeUntil.js
@@ -1,5 +1,5 @@
-const checkGreaterThan5 = x => x > 5 
-const lengthTenOrUnder = x => x.length <= 10
+const checkGreaterThan5 = x => x > 5;
+const lengthTenOrUnder = x => x.length <= 10;
 
 
 const takeUntil = function(arr, cb) {
@@ -7,12 +7,12 @@ const takeUntil = function(arr, cb) {
   for (let i = 0; i < arr.length; i++) {
     results.push(arr[i]);
     if (cb(arr[i])) {
-      return results
+      return results;
     }
-  };
-  return results
-}
+  }
+  return results;
+};
 
-console.log(takeUntil([1,2,3,4,5,6,7,8,9,10],checkGreaterThan5))
-console.log(takeUntil(['mary had a little lamb','bojack horseman','yeah','another long string to see if it works'],lengthTenOrUnder))
+console.log(takeUntil([1,2,3,4,5,6,7,8,9,10],checkGreaterThan5));
+console.log(takeUntil(['mary had a little lamb','bojack horseman','yeah','another long string to see if it works'],lengthTenOrUnder));
 

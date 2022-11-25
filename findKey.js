676882fdcb8d9c32065215282f9a95c064eef0a8
DASitby <davidsooley551@gmail.com>
2022-11-25 23:46:59 +0000

    linted and added another test case

--- findKey.js
+++ findKey.js
@@ -16,11 +16,19 @@ const findKey = function(obj, cb) {
   }
 };
 
-console.log(findKey({
+assertEqual((findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2))
+}, x => x.stars === 2)),'noma');
+assertEqual((findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 3 },
+  "elBulli":   { stars: 1 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)),'Ora');

94c19c4b59b0b523b8cef29c039b638aba2365a4
DASitby <davidsooley551@gmail.com>
2022-11-25 23:42:48 +0000

    implemented initial function logic

--- findKey.js
+++ findKey.js
@@ -5,3 +5,22 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const findKey = function(obj, cb) {
+  let output;
+  for (let key in obj) {
+    if (cb(obj[key])) {
+      output = key;
+      return output;
+    }
+  }
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))

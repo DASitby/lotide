dd3576413ff328593046f5c0cf4c78749d7b3586
DASitby <davidsooley551@gmail.com>
2022-12-07 14:19:46 +0000

    refactored tail to use require and module export, and seperated the test to its own file

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(input) {
   let output = input;
@@ -12,7 +6,4 @@ const tail = function(input) {
   return output;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;

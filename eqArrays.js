ed3feeb8cdc6b7764ce964e9a68d95b78c54e754
DASitby <davidsooley551@gmail.com>
2022-12-07 14:28:39 +0000

    refactored eqArrays to export function and seperated test into seperate file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -34,11 +26,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-//test code
-assertEqual(eqArrays([1,2,3],[1,2,3]),true);
-assertEqual(eqArrays([1,2,3],[1,2,3,4]),false);
-assertEqual(eqArrays([1,2,3],[1,1,3]),false);
-assertEqual(eqArrays(['david','andrew','sooley'],['david','andrew','sooley']),true);
-assertEqual(eqArrays([[[2, 3]], [4]], [[[2, 3]], [4]]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]),false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]),false);
+module.exports = eqArrays;

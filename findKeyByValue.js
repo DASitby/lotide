17332e2ec7a9ab5dd01dda869a9cbdc08391e97b
DASitby <davidsooley551@gmail.com>
2022-12-07 17:28:32 +0000

    refactored findKeyByValue to export, and wrote tests for it

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(object, search) {
   let output;
   for (let key in object) {
@@ -16,13 +8,4 @@ const findKeyByValue = function(object, search) {
   return output;
 };
 
-const bestTVShowsByGenre = {
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+module.exports = findKeyByValue;
---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,26 @@
+const assert = require('chai').assert;
+const findKeyByValue = require('../findKeyByValue');
+
+describe(`#findKeyByValue using "const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire" 
+  }`, () => {
+  const bestTVShowsByGenre = {
+    sciFi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+  it('returns "drama" when given "The Wire"', () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  });
+  it('returns undefined when given a key not present', () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+  });
+  it('returns sciFi when given "The Expanse"', () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
+  });
+  it('returns comedy when given "Brooklyn Nine-Nine"', () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+  });
+});

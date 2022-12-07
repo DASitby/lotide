30b8b967e0f360e673ff7c980f6ad4ac0d82a047
DASitby <davidsooley551@gmail.com>
2022-12-07 19:23:50 +0000

    added all functions to index

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const map = require('./map');
+const flatten = require('./flatten');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const letterPositions = require('./letterPositions');
 
 module.exports = {
   head,
   tail,
   middle,
-  
+  map,
+  flatten,
+  takeUntil,
+  without,
+  findKey,
+  findKeyByValue,
+  eqArrays,
+  eqObjects,
+  countOnly,
+  countLetters,
+  letterPositions,
 };
